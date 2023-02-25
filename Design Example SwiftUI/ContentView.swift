f7201b22bf5c204ccc53e17e439c7783e5769d89
Aleksandr Kh <akumasanta@gmail.com>
2023-02-25 16:08:29 +0600

    add text and logo

--- Design Example SwiftUI/ContentView.swift
+++ Design Example SwiftUI/ContentView.swift
@@ -4,18 +4,25 @@
 //
 //  Created by Александр Х on 25.02.2023.
 //
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, Aleks!")
+        HStack {
+            VStack { // вертикальный стак
+                Image(systemName: "globe")
+                    .imageScale(.large) // размер картинки
+                    .foregroundColor(.accentColor) // цвет
+                Text("Дизайн в SwiftUI!")
+                    .font(.title) // шрифт
+                    .fontWeight(.semibold) // жирность полужирный
+                    .foregroundColor(.black) // цвет
+                Text("Сертификат")
+                    .foregroundColor(Color("Primary"))
+            }
+            Image("Logo SwiftUI")
         }
-        .padding()
+        
     }
 }
 
