020e7c38584e7c80ed4c07873c2c2a364a822458
Aleksandr Kh <akumasanta@gmail.com>
2023-02-25 13:50:51 +0600

    Initial Commit

---
+++ Design Example SwiftUI/Design_Example_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_Example_SwiftUIApp.swift
+//  Design Example SwiftUI
+//
+//  Created by Александр Х on 25.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Design_Example_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
